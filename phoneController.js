c696bf6c18417e2f747f91c35ad2f5d7a640f835
Amlan Ghosh <amlan.ghosh01@gmail.com>
2016-02-19 01:10:41 +0530

    Added files via upload

---
+++ phoneController.js
@@ -0,0 +1,31 @@
+﻿var phoneapp = angular.module("phoneapp", []);
+var phone_data = ["Samsung", "Nokia", "Sony", "Microsoft", "HTC", "Apple"];
+var phoneController = function ($scope) {
+
+    $scope.phones = phone_data;
+
+
+};
+phoneapp.controller("phoneController", phoneController);
+
+var searchController = function ($scope, $routeParams) {
+
+    var flag = 0;
+    for (var i = 0; i < phones.length; i++) {
+        if (phone_data[i] == $routeParams.item) {
+            flag = 1;
+        }
+
+    }
+    if (flag == 1) {
+        $scope.stat = "Found";
+
+    }
+    else {
+
+        $scope.stat = "Not Found";
+    }
+
+
+};
+phoneapp.controller("searchController", ['$scope', '$routeParams', searchController]);
---
+++ script.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('myapp', ['ngRoute', 'phoneapp']);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when('/phones', {
+        templateUrl: 'phone-list.html',
+        controller: 'phoneController'
+
+    })
+    .when('/search', {
+        templateUrl: 'search.html',
+        controller: 'phoneController'
+    })
+    .when('/:item', {
+        templateUrl: 'phone-detail.html',
+        controller: 'searchController'
+
+    })
+    .otherwise({
+        redirectTo: '/phones'
+    })
+
+
+}]);
